6c38a68037d15629df36a0e033e3ca50f49ad3a7
mbilker <me@mbilker.us>
2015-12-31 01:36:02 -0500

    :arrow_up: personal edits for code style

--- src/smalltalk.native.js
+++ src/smalltalk.native.js
@@ -1,54 +1,44 @@
-(function(global) {
-    'use strict';
-    
-    if (typeof module !== 'undefined' && module.exports)
-        module.exports = Smalltalk();
-    else
-        global.smalltalk = Smalltalk();
-        
-    
-    function Smalltalk() {
-        if (!(this instanceof Smalltalk))
-            return new Smalltalk();
-        
-        this.alert          = (title, message) => {
-            let promise = new Promise(function(resolve) {
-                alert(message);
-                resolve();
-            });
-            
-            return promise;
-        };
-        
-        this.prompt         = (title, message, value, options) => {
-            let o       = options,
-                promise = new Promise(function(resolve, reject) {
-                    let noCancel = o && !o.cancel,
-                        result  = prompt(message, value);
-                    
-                    if (result !== null)
-                        resolve(result);
-                    else if (!noCancel)
-                        reject();
-                });
-            
-            return promise;
-        };
-        
-        this.confirm         = (title, message, options) => {
-            let o           = options,
-                noCancel    = o && !o.noCancel,
-                promise     = new Promise(function(resolve, reject) {
-                    let is = confirm(message);
-                    
-                    if (is)
-                        resolve();
-                    else if (!noCancel)
-                        reject();
-                });
-            
-            return promise;
-        };
+(function() {
+  'use strict';
+
+  function Smalltalk() {
+    if (!(this instanceof Smalltalk)) {
+      return new Smalltalk();
     }
-    
-})(typeof window !== 'undefined' && window);
+
+    this.alert = (title, message) => {
+      return new Promise(function(resolve) {
+        alert(message);
+        resolve();
+      });
+    };
+
+    this.prompt = (title, message, value, options) => {
+      return new Promise(function(resolve, reject) {
+        let noCancel = options && !options.cancel;
+        let result = prompt(message, value);
+
+        if (result !== null) {
+          resolve(result);
+        } else if (!noCancel) {
+          reject();
+        }
+      });
+    };
+
+    this.confirm = (title, message, options) => {
+      let noCancel = options && !options.noCancel;
+      return new Promise(function(resolve, reject) {
+        let is = confirm(message);
+
+        if (is) {
+          resolve();
+        } else if (!noCancel) {
+          reject();
+        }
+      });
+    };
+  }
+
+  module.exports = new Smalltalk();
+})();
